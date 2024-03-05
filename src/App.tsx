39d211982ee72d60d86a674a680840764684b0dd
Asia.Bielawska <joanna.bielawska8@gmail.com>
2024-03-05 12:34:48 +0100

    Add facts about cats

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
+import axios from "axios";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return <></>;
+  const [inputValue, setInputValue] = useState("");
+  const [arrFacts, setArrFacts] = useState<string[]>();
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+  const factsData = async () => {
+    try {
+      const response = await axios.get(
+        `https://meowfacts.herokuapp.com/?count=${inputValue}`
+      );
+      setArrFacts(response.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  console.log(arrFacts);
+
+  return (
+    <>
+      <div>
+        Type in the input number of the facts that you want to know about cats
+      </div>
+      <div>
+        <input value={inputValue} onChange={handleChange}></input>
+      </div>
+      <button onClick={factsData}>Click me!</button>
+      {arrFacts && (
+        <ol>
+          {arrFacts.map((el, index) => (
+            <li key={index}> {el}</li>
+          ))}
+        </ol>
+      )}
+    </>
+  );
 }
 
 export default App;
